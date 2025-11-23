fb78aa2ae0aeea7dd49833ff9fdef611b47916ff
sbf6606 <fraayadia@alum.us.es>
2025-11-24 00:49:40 +0100

    feat: base del módulo Fossils

--- app/modules/fossils/models.py
+++ app/modules/fossils/models.py
@@ -1,26 +1,27 @@
 from app import db
 
 
-class FossilFile(db.Model):
-    __tablename__ = 'fossil_file'
+class FossilsFile(db.Model):
+    __tablename__ = 'fossils_file'
     id = db.Column(db.Integer, primary_key=True)
     data_set_id = db.Column(db.Integer, db.ForeignKey("data_set.id"), nullable=False)
-    fm_meta_data_id = db.Column(db.Integer, db.ForeignKey("fm_meta_data.id"))
 
-    files = db.relationship("Hubfile", backref="fossil_file", lazy=True, cascade="all, delete")
-    fm_meta_data = db.relationship("FMMetaData", uselist=False, backref="fossil_file", cascade="all, delete")
+    fossils_meta_data_id = db.Column(db.Integer, db.ForeignKey("fossils_meta_data.id"))
+
+    files = db.relationship("Hubfile", backref="fossils_file", lazy=True, cascade="all, delete")
+    fossils_meta_data = db.relationship("FossilsMetaData", uselist=False, backref="fossils_file", cascade="all, delete")
 
     def __repr__(self):
         return f'Fossils<{self.id}>'
 
-class FossilMetaData(db.Model):
-    __tablename__ = 'fossil_meta_data'
+class FossilsMetaData(db.Model):
+    __tablename__ = 'fossils_meta_data'
     id = db.Column(db.Integer, primary_key=True)
     csv_filename = db.Column(db.String(120), nullable=False)
     title = db.Column(db.String(120), nullable=False)
     description = db.Column(db.Text, nullable=False)
 
     def __repr__(self):
-        return f'FossilMetaData<{self.title}>'
+        return f'FossilsMetaData<{self.title}>'
 
 
--- app/modules/fossils/repositories.py
+++ app/modules/fossils/repositories.py
@@ -1,7 +1,17 @@
-from app.modules.fossils.models import Fossils
+from sqlalchemy import func
+
+from app.modules.fossils.models import FossilsFile, FossilsMetaData
 from core.repositories.BaseRepository import BaseRepository
 
 
 class FossilsRepository(BaseRepository):
     def __init__(self):
-        super().__init__(Fossils)
+        super().__init__(FossilsFile)
+
+    def count_feature_models(self) -> int:
+        max_id = self.model.query.with_entities(func.max(self.model.id)).scalar()
+        return max_id if max_id is not None else 0
+
+class FossilsMetaDataRepository(BaseRepository):
+    def __init__(self):
+        super().__init__(FossilsMetaData)
