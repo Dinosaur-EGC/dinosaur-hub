950210232483196ef231c7e38af790a4049adfee
sbf6606 <fraayadia@alum.us.es>
2025-11-23 23:06:25 +0100

    fix: syntax error in fossils models

--- app/modules/fossils/models.py
+++ app/modules/fossils/models.py
@@ -13,7 +13,7 @@ class FossilFile(db.Model):
     def __repr__(self):
         return f'Fossils<{self.id}>'
 
-class FossilMetaData/db.Model):
+class FossilMetaData(db.Model):
     __tablename__ = 'fossil_meta_data'
     id = db.Column(db.Integer, primary_key=True)
     csv_filename = db.Column(db.String(120), nullable=False)
