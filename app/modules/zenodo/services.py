3d91a92aa5a3a83439a3866e8baf04020b92864b
sbf6606 <fraayadia@alum.us.es>
2025-11-24 11:26:56 +0100

    fix: cambiar featureModel por Fossils en el módulo Zenodo

--- app/modules/zenodo/services.py
+++ app/modules/zenodo/services.py
@@ -7,7 +7,7 @@ from flask import Response, jsonify
 from flask_login import current_user
 
 from app.modules.dataset.models import DataSet
-from app.modules.featuremodel.models import FeatureModel
+from app.modules.fossils.models import FossilsFile
 from app.modules.zenodo.repositories import ZenodoRepository
 from core.configuration.configuration import uploads_folder_name
 from core.services.BaseService import BaseService
@@ -178,7 +178,7 @@ class ZenodoService(BaseService):
             raise Exception(error_message)
         return response.json()
 
-    def upload_file(self, dataset: DataSet, deposition_id: int, feature_model: FeatureModel, user=None) -> dict:
+    def upload_file(self, dataset: DataSet, deposition_id: int, fossils_file: FossilsFile, user=None) -> dict:
         """
         Upload a file to a deposition in Zenodo.
 
@@ -190,10 +190,10 @@ class ZenodoService(BaseService):
         Returns:
             dict: The response in JSON format with the details of the uploaded file.
         """
-        uvl_filename = feature_model.fm_meta_data.uvl_filename
-        data = {"name": uvl_filename}
+        csv_filename = fossils_file.fossils_meta_data.csv_filename
+        data = {"name": csv_filename}
         user_id = current_user.id if user is None else user.id
-        file_path = os.path.join(uploads_folder_name(), f"user_{str(user_id)}", f"dataset_{dataset.id}/", uvl_filename)
+        file_path = os.path.join(uploads_folder_name(), f"user_{str(user_id)}", f"dataset_{dataset.id}/", csv_filename)
         files = {"file": open(file_path, "rb")}
 
         publish_url = f"{self.ZENODO_API_URL}/{deposition_id}/files"
