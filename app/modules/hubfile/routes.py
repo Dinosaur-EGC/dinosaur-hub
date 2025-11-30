ff9c10975ccd51760265b8a2851f8b4ede1e9574
Javier Luque Ruiz <javluqrui@alum.us.es>
2025-11-30 10:50:41 +0100

    feat: Ver los detalles de los csv

--- app/modules/hubfile/routes.py
+++ app/modules/hubfile/routes.py
@@ -45,7 +45,7 @@ def download_file(file_id):
 
     return resp
 
-
+'''
 @hubfile_bp.route("/file/view/<int:file_id>", methods=["GET"])
 def view_file(file_id):
     file = HubfileService().get_or_404(file_id)
@@ -93,3 +93,53 @@ def view_file(file_id):
             return jsonify({"success": False, "error": "File not found"}), 404
     except Exception as e:
         return jsonify({"success": False, "error": str(e)}), 500
+'''
+
+@hubfile_bp.route("/file/view/<int:file_id>", methods=["GET"])
+def view_file(file_id):
+    file = HubfileService().get_or_404(file_id)
+    filename = file.name
+
+    directory_path = f"uploads/user_{file.fossils_file.data_set.user_id}/dataset_{file.fossils_file.data_set_id}/"
+    parent_directory_path = os.path.dirname(current_app.root_path)
+    file_path = os.path.join(parent_directory_path, directory_path, filename)
+
+    try:
+        if os.path.exists(file_path):
+            with open(file_path, "r") as f:
+                content = f.read()
+
+            user_cookie = request.cookies.get("view_cookie")
+            if not user_cookie:
+                user_cookie = str(uuid.uuid4())
+
+            # Check if the view record already exists for this cookie
+            existing_record = HubfileViewRecord.query.filter_by(
+                user_id=current_user.id if current_user.is_authenticated else None,
+                file_id=file_id,
+                view_cookie=user_cookie,
+            ).first()
+
+            if not existing_record:
+                # Register file view
+                new_view_record = HubfileViewRecord(
+                    user_id=current_user.id if current_user.is_authenticated else None,
+                    file_id=file_id,
+                    view_date=datetime.now(),
+                    view_cookie=user_cookie,
+                )
+                db.session.add(new_view_record)
+                db.session.commit()
+
+            # Prepare response
+            response = jsonify({"success": True, "content": content})
+            if not request.cookies.get("view_cookie"):
+                response = make_response(response)
+                response.set_cookie("view_cookie", user_cookie, max_age=60 * 60 * 24 * 365 * 2)
+
+            return response
+        else:
+            return jsonify({"success": False, "error": "File not found"}), 404
+    except Exception as e:
+        return jsonify({"success": False, "error": str(e)}), 500
+
