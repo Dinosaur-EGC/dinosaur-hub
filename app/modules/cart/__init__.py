98e612d7054429d261599da88c26e536332f06ad
Javier Luque Ruiz <javluqrui@alum.us.es>
2025-12-08 11:37:23 +0100

    feat: Contador del carrito

--- app/modules/cart/__init__.py
+++ app/modules/cart/__init__.py
@@ -1,3 +1,17 @@
 from core.blueprints.base_blueprint import BaseBlueprint
+from flask_login import current_user
 
 cart_bp = BaseBlueprint("cart", __name__, template_folder="templates", url_prefix='/cart')
+
+@cart_bp.app_context_processor
+def inject_cart_count():
+    cart_count = 0
+    if current_user.is_authenticated:
+        try:
+            from .services import CartService
+            service = CartService()
+            items = service.get_cart_items(current_user.id)
+            cart_count = len(items)
+        except Exception:
+            cart_count = 0
+    return {'cart_count': cart_count}
