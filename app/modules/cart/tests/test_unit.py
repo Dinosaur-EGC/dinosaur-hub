1648ab8812aa05b65a12a584afae8ee712faa62d
Javier Luque Ruiz <javluqrui@alum.us.es>
2025-12-09 20:49:40 +0100

    Unit Tests de get_cart_items

--- app/modules/cart/tests/test_unit.py
+++ app/modules/cart/tests/test_unit.py
@@ -87,3 +87,32 @@ def test_remove_item_database_error(cart_service):
     assert status == 500
     assert "Internal error" in result["error"]
     cart_service.cart_repository.session.rollback.assert_called_once()
+
+#TEST PARA GET_CART_ITEMS   
+
+def test_get_cart_items(cart_service):
+    mock_cart_items = [MagicMock(hubfile_id=201), MagicMock(hubfile_id=202)]
+    cart_service.cart_repository.get_items_by_user.return_value = mock_cart_items
+
+    mock_hubfiles = [MagicMock(id=201, name="Fossil 1"), MagicMock(id=202, name="Fossil 2")]
+    cart_service.hubfile_repository.filter_by_ids.return_value = mock_hubfiles
+
+    result = cart_service.get_cart_items(user_id=1)
+
+    assert len(result) == 2
+    assert result == mock_hubfiles
+
+    cart_service.cart_repository.get_items_by_user.assert_called_once_with(1)
+    cart_service.hubfile_repository.filter_by_ids.assert_called_once_with([201, 202])
+
+def test_get_cart_items_empty(cart_service):
+    cart_service.cart_repository.get_items_by_user.return_value = []
+
+    cart_service.hubfile_repository.filter_by_ids.return_value = []
+
+    result = cart_service.get_cart_items(user_id=1)
+
+    assert result == []
+    cart_service.cart_repository.get_items_by_user.assert_called_once_with(1)
+    cart_service.hubfile_repository.filter_by_ids.assert_called_once_with([])
+
